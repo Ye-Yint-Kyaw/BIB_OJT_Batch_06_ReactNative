696df631a2eacb67262e897b35664e67b6efef4d
Ye-Yint-Kyaw <yeyintkyaw.sgg@gmail.com>
2022-12-12 16:09:14 +0630

    done

--- Day_03/App.tsx
+++ Day_03/App.tsx
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const fruits = ['Apple', 'Orange','Banana', 'Grape', 'Tomato'];
+  const [show, setShow] = useState<string[]>([]);
+  const upper_item = fruits.map(up => up.toUpperCase());
+  const [value, onChangeText] = useState(''); 
+  const [filtered, setFiltered] = useState<string[]>([]);
+  
   return (
     <View style={styles.container}>
       <Text>Original Array</Text>
-      <Text>[Apple, Orange,Banana, Grape, Tomato]</Text>
-      <Text>Original Array</Text>
+      <Text>{fruits}</Text>
+      <view>
+        <Button
+        title='CHANGE TO UPPER CASE'
+        onPress = {() => setShow(upper_item)}
+        />
+      </view>
+      <Text>Upper Case Array</Text>
+      <Text>{show}</Text>
+      
+      <View>
+        <TextInput
+        placeholder='Enter something to filter'
+        onChangeText={ text => onChangeText(text)}
+        value = {value}
+        />
+        <Button
+        title='FILTER ARRAY WITH INPUT'
+        onPress={ () => setFiltered(fruits.filter( val => val.includes(value) ))}
+        />
+      </View>
+      <Text>Filter Array</Text>
+      <Text>{filtered}</Text>
       <StatusBar style="auto" />
     </View>
   );
